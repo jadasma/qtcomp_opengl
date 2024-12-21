c58abf7f738461e159cbe8533aba343c3fffaf66
Mounirba77 <118421059+Mounirba77@users.noreply.github.com>
2024-12-21 14:07:25 +0100

    Update starrenderer.h

--- starrenderer.h
+++ starrenderer.h
@@ -1,12 +1,15 @@
 #ifndef STARRENDERER_H
 #define STARRENDERER_H
 
+#include <QtOpenGL>
 #include <QOpenGLFunctions_3_3_Core>
 #include <QOpenGLFunctions>
 #include <QOpenGLShaderProgram>
 #include <QQuickFramebufferObject>
 #include <QOpenGLFramebufferObject>
 #include <QOpenGLFramebufferObjectFormat>
+#include <QMatrix4x4>
+#include <QQuickItem>
 
 class StarRenderer : public QQuickFramebufferObject::Renderer, protected QOpenGLFunctions_3_3_Core {
 public:
@@ -16,6 +19,7 @@ public:
     void render() override;
     QOpenGLFramebufferObject *createFramebufferObject(const QSize &size) override;
     void synchronize(QQuickFramebufferObject *item) override;
+    // const bool shadersverif;
 
 private:
     QOpenGLShaderProgram m_shaderProgram;
