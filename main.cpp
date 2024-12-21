4fea68c1b6359995cbe02ab6559c502055a74617
Mounirba77 <118421059+Mounirba77@users.noreply.github.com>
2024-12-21 14:08:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,23 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "StarRenderer.h"
-
+#include <QtOpenGL>
+#include <QOpenGLWindow>
+#include <QOpenGLFunctions_3_3_Core>
+#include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QQuickFramebufferObject>
+#include <QOpenGLFramebufferObject>
+#include <QOpenGLFramebufferObjectFormat>
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+    // Query OpenGL version and renderer
+    qDebug() << "OpenGL Version: " << (const char*)glGetString(GL_VERSION);
+    qDebug() << "OpenGL Renderer: " << (const char*)glGetString(GL_RENDERER);
+    qDebug() << "123";
     QGuiApplication app(argc, argv);
     qmlRegisterType<StarItem>("CustomComponents", 1, 0, "StarItem");
     QQmlApplicationEngine engine;
